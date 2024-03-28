0d9a9ad299adbc93cc01a80823430dcece389704
cguz14 <cguz14@gmail.com>
2024-03-28 01:37:10 -0500

    basic site functional

--- server.py
+++ server.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, session, redirect, jsonify
 from flask_bcrypt import Bcrypt
-from model import connect_to_db
-from pprint import pformat, pprint
-from bs4 import BeautifulSoup as bs
-from email_validator import validate_email, EmailNotValidError
+# from model import connect_to_db
 
 from jinja2 import StrictUndefined
 
 import os
-import json
-import requests
-import random
 
 app = Flask(__name__)
 flask_bcrypt = Bcrypt(app)
@@ -24,8 +18,7 @@ def homepage():
 	return render_template('homepage.html')
 
 if __name__ == "__main__":
-	connect_to_db(app)
-	app.run() # debug=True useful on pc side while still 
-	#	testing and building. Need to ensure that server side maintains lack of debug mode.
-	asdfasdfasdf
-	asdfa
+	# connect_to_db(app)
+	app.run() 
+	debug=True  # Useful on pc side while still testing and building. 
+	            # Need to ensure that server side maintains lack of debug mode.
