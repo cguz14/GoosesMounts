c7f45a4793e448df085dd5f1aec50f7578e9b2a4
cguz14 <cguz14@gmail.com>
2024-05-08 17:30:21 -0500

    added mounts, accys, search brand to navbar. Adding further dropdowns to branch search

--- server.py
+++ server.py
@@ -17,8 +17,26 @@ def homepage():
 
 	return render_template('homepage.html')
 
+@app.route('/mounts')
+def all_mounts():
+	"""Show All Mounts"""
+
+	return render_template('all_mounts.html')
+
+@app.route('/accessories')
+def accessories():
+	"""Show All Mounts"""
+
+	return render_template('accessories.html')
+
+@app.route('/Sony-32R400A')
+def brand_search():
+	"""Show All Mounts"""
+
+	return render_template('Sony_32R400A.html')
+
 if __name__ == "__main__":
 	# connect_to_db(app)
-	app.run() 
+	app.run()
 	debug=True  # Useful on pc side while still testing and building. 
 	            # Need to ensure that server side maintains lack of debug mode.
